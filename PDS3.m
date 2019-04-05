368b89f7bf4ed74afa0fc98ff0ba4cdb9cff8080
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2019-04-05 13:55:46 -0600

    Play echoes

--- PDS3.m
+++ PDS3.m
@@ -1,7 +1,8 @@
 %Reads/Loads sound
 [x,fs] = audioread('noti_res.wav');
-x = x(:,1);
 x_sound = audioplayer(x, fs);
+play(x_sound);
+x = x(:,1);
 %Values for echo
 delay = 2000;
 echo = 5;
@@ -19,7 +20,7 @@ h1(1 , delay) = alpha;
 h2 = zeros(1, size);
 h2(1 , 1) = 1;
 for k = 1 : echo;
-    h2(1 , (k * delay)) = (1 - alpha ^ 2) * a ^ (k - 2);
+    h2(1 , (k * delay)) = (1 - alpha ^ 2) * alpha ^ (k - 2);
 end
 %Impulse response H3
 h3 = zeros(1, size);
@@ -41,11 +42,11 @@ h2_fft = abs(fft(h2, length(x)));
 h3_fft = abs(fft(h3, length(x)));
 w = linspace(0, fs/2, length(x) / 2);
 %Graphs of Fourier Transform of impulse response
-plot(w, h1_fft(1 : lenght(x) / 2), 'Linewidth', 4);
+plot(w, h1_fft(1 : length(x) / 2), 'Linewidth', 4);
 title('H1 Fourier Transform');
-plot(w, h2_fft(1 : lenght(x) / 2), 'Linewidth', 4);
+plot(w, h2_fft(1 : length(x) / 2), 'Linewidth', 4);
 title('H2 Fourier Transform');
-plot(w, h3_fft(1 : lenght(x) / 2), 'Linewidth', 4);
+plot(w, h3_fft(1 : length(x) / 2), 'Linewidth', 4);
 title('H3 Fourier Transform');
 %Filters for echoed signal
 y1_echo = filter(h1, -1, x);
@@ -60,3 +61,10 @@ plot(y2_echo);
 title('Y2 Echoed');
 plot(y3_echo);
 title('Y3 Echoed');
+%Play echos
+y1_sound = audioplayer(y1_echo, fs);
+y2_sound = audioplayer(y2_echo, fs);
+y3_sound = audioplayer(y3_echo, fs);
+play(y1_sound);
+play(y2_sound);
+play(y3_sound);
